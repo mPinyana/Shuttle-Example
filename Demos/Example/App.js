9a60211f11bb115d1744f2c7a0e56e4229d41a0c
Your Name <you@example.com>
2024-08-04 22:56:06 +0200

    sign up page UI

--- Demos/Example/App.js
+++ Demos/Example/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginPage from './app/Pages/LoginPage';
 import UserProfile from  './app/Pages/DisplayprofilePage';
 import HomePage from './app/Pages/HomePage.js';
+import SignUpScreen from './app/Pages/SignUpScreen.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -11,9 +12,10 @@ export default function App() {
   //console.log(useDimensions());
   return (
     <NavigationContainer>  
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen name= "Login" component={LoginPage} options={{ headerShown: false}} /> 
-          <Stack.Screen name = 'Home Page' component = {HomePage}/>
+        {/* <Stack.Navigator initialRouteName='Login'> */}
+        <Stack.Navigator initialRouteName='SignUp'>
+          <Stack.Screen name= "SignUp" component={SignUpScreen} options={{ headerShown: false}} /> 
+          {/* <Stack.Screen name = 'Home Page' component = {HomePage}/> */}
           <Stack.Screen name="Profile" component={UserProfile} options={{ headerShown: false}} />
         </Stack.Navigator>
     </NavigationContainer>
--- Demos/Example/app/Pages/SignUpScreen.js
+++ Demos/Example/app/Pages/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet ,View, Text,TextInput,Platform, Button,TouchableOpacity} from 'react-native'; 
+import { StyleSheet ,View, Text,TextInput,TouchableOpacity} from 'react-native'; 
 
 function SignUpScreen(){
 
@@ -8,30 +8,39 @@ function SignUpScreen(){
         <View style={styles.container}>
         
             <View style={styles.header}>
-            <Text style={styles.headerText}>
-            Sign Up
-            </Text>
-            <Text style={styles.subheadingText}>
-                Create your account
-            </Text>
-
-            <View style={styles.inputsContainer}>
-                <TextInput style={styles.input}
-                    placeholder="Enter name"
-                />
-                <TextInput style={styles.input}
-                    placeholder="Enter surname"
-                />
-                <TextInput style={styles.input}
-                    placeholder="Enter email"
-                />
-                <TextInput style={styles.input}
-                    placeholder="Enter password"
-                />
-                
+                <Text 
+                style={styles.headerText}>
+                Create Profile
+                </Text>
             </View>
 
+            <View style={styles.inputContainerA}>
+
+                <TextInput style={styles.input}
+                    placeholder="Name"/>
+
+                <TextInput style={styles.input}
+                    placeholder="Surname"/>
             </View>
+
+            <View style={styles.inputContainerB}>
+                <TextInput style={styles.inputB}
+                    placeholder="Email"/>
+
+                <TextInput style={styles.inputB}
+                    placeholder="Password"/>
+          
+            </View>
+
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity 
+            activeOpacity={0.8}
+            style={styles.buttonSignup}  
+            >
+            <Text style={styles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
+
+        </View>
 
         </View>
 
@@ -47,15 +56,86 @@ const styles = StyleSheet.create({
     },
 
     header:{
-        height: 0.2,
+        height: 140,
         width:'100%',
-        backgroundColor: '#00308F',
-        justifyContent:'space-evenly',
+        justifyContent:'flex-end',
         alignItems:'center',
+        paddingBottom: 20,
 
     },
 
     headerText:{
-
+        fontSize: 24,
+        fontWeight: 'bold',
+        alignItems: '',
     },
+
+    inputContainerA:{
+    flexDirection: 'row',
+    height: 90,
+    width:'100%',
+    justifyContent:'space-evenly',
+    alignItems: 'center',
+    },
+
+    inputContainerB:{
+        height: 90,
+        width:'100%',
+        justifyContent:'space-evenly',
+        alignItems: 'flex-start',
+        paddingLeft: 14,
+        },
+
+    input:{
+        height: '40%',
+        width: '45%',
+        padding: 10,
+        paddingLeft: 12,
+        borderRadius: 20,
+        justifyContent: 'space-evenly',
+        marginVertical: '5%',
+        backgroundColor: '#d9d9d9',
+        borderWidth: 0.2,
+    },
+
+    inputB:{
+        height: '40%',
+        width: '80%',
+        padding: 10,
+        paddingLeft: 12,
+        borderRadius: 20,
+        justifyContent: 'space-evenly',
+        marginVertical: '8%',
+        backgroundColor: '#d9d9d9',
+        borderWidth: 0.2,
+    },
+
+    buttonContainer: {
+        height: 120,
+        width:'100%',
+        justifyContent:'space-evenly',
+        alignItems: 'center',
+    },
+
+    buttonSignup:{
+        height: '35%',
+        width:'60%',
+        backgroundColor: '#FF5757',
+        borderRadius: 20,
+        justifyContent:'space-evenly',
+        alignItems: 'center',
+        marginVertical: 10, 
+    },
+
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
+
+
+
+
+   
 })
